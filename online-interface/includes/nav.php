4638d7dc2069821bb8f04111f188533673608f2b
Celia Ludwinski <celia.ludwinski@gmail.com>
2018-07-19 15:19:33 -0700

    clean up

--- online-interface/includes/nav.php
+++ online-interface/includes/nav.php
@@ -1,5 +1,5 @@
 <ul class="nav navbar-nav navbar-right">
-  <li><a href="/Parlancr/theory.php">Theory</a></li>
-  <li><a href="/Parlancr/dev.php">Development</a></li> <!-- learnings? -->
-  <li><a href="/Parlancr/about-us.php">About Us</a></li>
+  <li><a href="theory.php">Theory</a></li>
+  <li><a href="dev.php">Development</a></li> <!-- learnings? -->
+  <li><a href="about-us.php">About Us</a></li>
 </ul>
